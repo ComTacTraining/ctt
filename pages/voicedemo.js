64e606da07c82670c5a1fa9b3bfde071d68e625d
Jeremy Moseley <jeremymoseley@me.com>
2021-02-14 08:25:16 -0800

    Transcribe access

--- pages/voicedemo.js
+++ pages/voicedemo.js
@@ -1,11 +1,16 @@
 import VoiceDemo from 'components/VoiceDemo'
 import { withGuestAuth } from 'components/Auth/HOC'
 
-const VoiceDemoPage = ({ accessId, secretKey}) => <VoiceDemo accessId={accessId} secretKey={secretKey} />
+const VoiceDemoPage = ({ accessId, secretKey }) => (
+  <VoiceDemo accessId={accessId} secretKey={secretKey} />
+)
 
 export const getServerSideProps = async () => {
-    return {
-        props: {accessId: process.env.AWS_ACCESS_ID, secretKey: process.env.AWS_SECRET_KEY}
+  return {
+    props: {
+      accessId: process.env.TRANSCRIBE_ACCESS_ID,
+      secretKey: process.env.TRANSCRIBE_SECRET_KEY
     }
+  }
 }
 export default withGuestAuth(VoiceDemoPage)

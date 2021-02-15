e92a406ed6f3584200390a0bba87d9142d31cafb
softwarebusiness588 <softwarebusiness588@gmail.com>
2021-02-14 23:56:30 -0700

    create the api for presigned url

--- pages/voicedemo.js
+++ pages/voicedemo.js
@@ -1,11 +1,6 @@
 import VoiceDemo from 'components/VoiceDemo'
 import { withGuestAuth } from 'components/Auth/HOC'
 
-const VoiceDemoPage = ({ accessId, secretKey}) => <VoiceDemo accessId={accessId} secretKey={secretKey} />
+const VoiceDemoPage = () => <VoiceDemo />
 
-export const getServerSideProps = async () => {
-    return {
-        props: {accessId: process.env.AWS_ACCESS_ID, secretKey: process.env.AWS_SECRET_KEY}
-    }
-}
 export default withGuestAuth(VoiceDemoPage)

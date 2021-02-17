7412bb778f566c0c8b0dc7b75704c3dc6048414b
Jeremy Moseley <jeremymoseley@me.com>
2021-02-16 16:24:21 -0800

    Add env variables to next config

--- next.config.js
+++ next.config.js
@@ -2,9 +2,17 @@ const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
-  webpack: config => {
+  webpack: (config) => {
     config.resolve.modules.push(path.resolve('./'))
 
     return config
+  },
+  target: 'serverless',
+  env: {
+    STRIPE_PUBLISHABLE_KEY: process.env.STRIPE_PUBLISHABLE_KEY,
+    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+    STRIPE_PRICE_ID: process.env.STRIPE_PRICE_ID,
+    TRANSCRIBE_ACCESS_ID: process.env.TRANSCRIBE_ACCESS_ID,
+    TRANSCRIBE_SECRET_KEY: process.env.TRANSCRIBE_SECRET_KEY
   }
 }

f6e7dbabacdc42e6814af1fdc0e728c3229e8d6f
Jeremy Moseley <jeremymoseley@me.com>
2021-02-17 22:24:21 -0800

    Amplify update
    Potential suggestion from https://github.com/aws-amplify/amplify-js/issues/4315

--- pages/_app.js
+++ pages/_app.js
@@ -15,11 +15,13 @@ import { Provider } from 'react-redux'
 import { createStore } from 'store'
 import Amplify from 'aws-amplify'
 import Auth from '@aws-amplify/auth'
-import Predictions, { AmazonAIPredictionsProvider } from '@aws-amplify/predictions'
+import Predictions, {
+  AmazonAIPredictionsProvider
+} from '@aws-amplify/predictions'
 
 import config from 'aws-exports'
 Amplify.configure({
-  config,
+  ...config,
   ssr: true
 })
 // Amplify.register(Predictions)
@@ -44,27 +46,50 @@ const MyApp = ({ Component, pageProps }) => {
     handleClearError
   } = useAmplifyAuth()
 
-  const value = React.useMemo(() => (
-    { user, isLoading, isMember, isAdmin, errorMessage, handleSignOut, handleClearError }
-  ), [user, isLoading, isMember, isAdmin, errorMessage, handleSignOut, handleClearError])
+  const value = React.useMemo(
+    () => ({
+      user,
+      isLoading,
+      isMember,
+      isAdmin,
+      errorMessage,
+      handleSignOut,
+      handleClearError
+    }),
+    [
+      user,
+      isLoading,
+      isMember,
+      isAdmin,
+      errorMessage,
+      handleSignOut,
+      handleClearError
+    ]
+  )
 
   return (
     <>
       <Head>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-        <meta name="description" content="Command Tactical Training is a management training corporation specifically designed to improve emergency services throughout the nation by providing ongoing, realistic, verifiable training. &nbsp;This training provides each user with the opportunity to experience the stress and gain the skills necessary to handle any emergency. &nbsp;The products have been developed by some of the top leaders […]" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name='viewport'
+          content='minimum-scale=1, initial-scale=1, width=device-width'
+        />
+        <meta
+          name='description'
+          content='Command Tactical Training is a management training corporation specifically designed to improve emergency services throughout the nation by providing ongoing, realistic, verifiable training. &nbsp;This training provides each user with the opportunity to experience the stress and gain the skills necessary to handle any emergency. &nbsp;The products have been developed by some of the top leaders […]'
+        />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
       <Provider store={store}>
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <UserContext.Provider value={value}>
             <AppBar />
-              <Box my={4}>
-                <Container maxWidth='xl' component='main'>
-                  <Component {...pageProps} />
-                </Container>
-              </Box>
+            <Box my={4}>
+              <Container maxWidth='xl' component='main'>
+                <Component {...pageProps} />
+              </Container>
+            </Box>
             <Footer />
           </UserContext.Provider>
         </ThemeProvider>

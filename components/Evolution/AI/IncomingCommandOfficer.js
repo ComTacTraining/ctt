dd0acaf217c64abab2094cad9a9bd1e28e898ebb
Jeremy Moseley <jeremymoseley@me.com>
2021-06-17 12:45:18 -0700

    Dispatch the arrival time in seconds
    Closes #58

--- components/Evolution/AI/IncomingCommandOfficer.js
+++ components/Evolution/AI/IncomingCommandOfficer.js
@@ -51,9 +51,7 @@ const IncomingCommandOfficer = () => {
             Math.random() * (maxSecs - minArrivalSeconds + 1) +
               minArrivalSeconds
           ) * 1000
-      dispatch(
-        aiActions.addIncomingCommandArrival({ arrival: Date.now() + timeout })
-      )
+      dispatch(aiActions.addIncomingCommandArrival(Date.now() + timeout))
       interval = setTimeout(() => {
         setSpeak({
           text: `${firstOnScene} from ${incomingCommandOfficer} can we do a face to face?`,

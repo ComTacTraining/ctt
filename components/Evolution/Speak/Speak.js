aee0e32b62fdb9b9395dbc5b91e74047667097e0
Jeremy Moseley <jeremymoseley@me.com>
2021-06-26 13:51:08 -0700

    Speech cooldown only if command possible
    Closes #60

--- components/Evolution/Speak/Speak.js
+++ components/Evolution/Speak/Speak.js
@@ -4,11 +4,17 @@ import { addToLog, updateTextToSpeech } from 'store/actions/ai'
 
 const Speak = () => {
   const dispatch = useDispatch()
-  const { waitingToBeSpoken, radioInUse } = useSelector((state) => state.ai)
+  const {
+    waitingToBeSpoken,
+    radioInUse,
+    firstAlarmAnnounced,
+    faceToFaceCompleted
+  } = useSelector((state) => state.ai)
 
   useEffect(() => {
     let cooldown
     if (!radioInUse && waitingToBeSpoken.length > 0) {
+      const waitTime = firstAlarmAnnounced && !faceToFaceCompleted ? 2000 : 10
       cooldown = setTimeout(() => {
         const nextSpeech = waitingToBeSpoken[0]
         const { label, text, voice, meta } = nextSpeech
@@ -16,10 +22,16 @@ const Speak = () => {
 
         dispatch(updateTextToSpeech({ text, voice, meta }))
         dispatch(addToLog({ timestamp, label, text }))
-      }, 2000)
+      }, waitTime)
     }
     return () => clearTimeout(cooldown)
-  }, [waitingToBeSpoken, radioInUse, dispatch])
+  }, [
+    waitingToBeSpoken,
+    radioInUse,
+    firstAlarmAnnounced,
+    faceToFaceCompleted,
+    dispatch
+  ])
 
   return <div />
 }

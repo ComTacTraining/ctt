f10d16008b91bb3e4ab8b78e6ca93e40b62b0cbc
Jeremy Moseley <jeremymoseley@me.com>
2021-07-23 10:47:40 -0700

    Sign up loading state
    Introduce isLoading UserContext variable and move user demo route push to a useEffect 
    Closes #120

--- pages/signup.js
+++ pages/signup.js
@@ -2,12 +2,13 @@ import { AmplifyAuthenticator, AmplifyContainer, AmplifySignUp } from '@aws-ampl
 import Alert from '@material-ui/lab/Alert'
 import Styles from 'aws/Styles'
 import { UserContext } from 'components/Auth/UserContext'
+import Loading from 'components/Loading'
 import { useRouter } from 'next/router'
 import * as React from 'react'
 
 const SignUpPage = () => {
   const router = useRouter()
-  const { user, errorMessage, handleClearError }  = React.useContext(UserContext)
+  const { user, isLoading, errorMessage, handleClearError }  = React.useContext(UserContext)
   const [displayedError, setDisplayedError] = React.useState('')
 
   React.useEffect(() => {
@@ -17,13 +18,20 @@ const SignUpPage = () => {
       } else if (/username.*expression/i.test(errorMessage)) {
         setDisplayedError('Username may not contain spaces')
       } else {
-        setDisplayedError(errorMessage)
+        setDisplayedError(errorMessage.toString())
       }
     }
   }, [errorMessage])
+
+  React.useEffect(() => {
+    if (user) {
+      router.push('/demo')
+    }
+  }, [user])
   
-  return user ? router.push('/demo') : (
+  return user ? null : (
     <>
+      {isLoading && <Loading />}
       {displayedError !== '' && (
         <Alert severity='warning' onClose={handleClearError}>
           {displayedError}
